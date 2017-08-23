a0f698ecd0642c8e7f2ff499f9fd082937d05aba
吴谦 <tate@zoeker.cn>
2017-08-23 13:10:33 +0800

    111

---
+++ MyApplication3/app/src/main/java/text/com/testandroid/Calculator.java
@@ -0,0 +1,24 @@
+package text.com.testandroid;
+
+/**
+ * Created by wuqian on 2017/8/18.
+ */
+
+public class Calculator {
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double substract(double a, double b){
+        return a-b;
+    }
+
+    public double divide(double a, double b){
+        return a/b;
+    }
+
+    public double multiply(double a, double b){
+        return a*b;
+    }
+}
